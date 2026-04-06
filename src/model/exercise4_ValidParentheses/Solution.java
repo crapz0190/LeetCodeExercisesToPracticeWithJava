1251bd03aacca74e3df7e1069fa20d59510f363c
crapz <crapz0190@gmail.com>
2026-04-06 14:56:49 -0300

    feat: solve Valid Parentheses using a Stack for LIFO matching

---
+++ src/model/exercise4_ValidParentheses/Solution.java
@@ -0,0 +1,22 @@
+package model.exercise4_ValidParentheses;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == ']' && top != '[' || c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
