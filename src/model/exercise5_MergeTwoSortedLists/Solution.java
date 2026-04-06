04171ae01d84239a8439796b1330fe318e60ff20
crapz <crapz0190@gmail.com>
2026-04-06 14:58:28 -0300

    feat: merge two sorted linked lists using a dummy node and iterative pointers

---
+++ src/model/exercise5_MergeTwoSortedLists/Solution.java
@@ -0,0 +1,79 @@
+package model.exercise5_MergeTwoSortedLists;
+
+public class Solution {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Main {
+    // 3. Metodo auxiliar para imprimir la lista fácilmente
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + (head.next != null ? " -> " : ""));
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // 4. Metodo Main para pruebas
+    public static void main(String[] args) {
+        // Crear Lista 1: 1 -> 2 -> 4
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        // Crear Lista 2: 1 -> 3 -> 4
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        System.out.print("Lista 1: ");
+        printList(l1);
+        System.out.print("Lista 2: ");
+        printList(l2);
+
+        // Fusionar
+        ListNode result = Solution.mergeTwoLists(l1, l2);
+
+        System.out.print("Resultado: ");
+        printList(result);
+    }
+}
