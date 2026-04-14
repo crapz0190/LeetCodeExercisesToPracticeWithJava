e22369b4c37d6dd365a15fd9016aed44492baa45
crapz <crapz0190@gmail.com>
2026-04-13 21:37:12 -0300

    Subject: Implement Search Insert Position using binary search.
     Body: - Added searchInsert method to find the index of a target value in a sorted array. - Implemented binary search logic with O(log n) time complexity. - Handled insertion index logic by returning the left pointer when the target is not found. - Included a Main class for local testing.

---
+++ src/model/exercise9_SearchInsertPosition/Solution.java
@@ -0,0 +1,30 @@
+package model.exercise9_SearchInsertPosition;
+
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 5, 7, 9, 11, 13, 23, 43, 51};
+        int target = 6;
+        System.out.println(solution.searchInsert(nums, target));
+    }
+}
