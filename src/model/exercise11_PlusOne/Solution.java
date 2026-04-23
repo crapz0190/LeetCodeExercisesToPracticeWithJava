033de747d4d8d648adb340bb41368ec55419e46b
crapz <crapz0190@gmail.com>
2026-04-23 19:02:39 -0300

    feat: implement logic to handle Plus One for integer array.
    - Added iterative solution to increment digits from right to left. - Handled carry-over scenario when digits are 9. - Added logic to handle array resizing (e.g., 99 -> 100).

---
+++ src/model/exercise11_PlusOne/Solution.java
@@ -0,0 +1,29 @@
+package model.exercise11_PlusOne;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        int[] digits = {2, 3, 1, 9, 9};
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.plusOne(digits)));
+    }
+}
