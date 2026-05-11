93698376e219a73f8c213a534ca6c5f55b09c976
crapz <crapz0190@gmail.com>
2026-05-11 19:42:06 -0300

    feat: solve 'Remove Duplicates from Sorted List' using single pointer

---
+++ src/model/exercise14_RemoveDuplicatesFromSortedList/Solution.java
@@ -0,0 +1,71 @@
+package model.exercise14_RemoveDuplicatesFromSortedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // Si la lista está vacía o tiene un solo nodo, no hay duplicados
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                // Si el valor es igual al siguiente, saltamos el nodo duplicado
+                current.next = current.next.next;
+            } else {
+                // Si son diferentes, avanzamos al siguiente nodo
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return val + " -> " +  next ;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        System.out.println(head);
+        System.out.println(s.deleteDuplicates(head));
+    }
+}
